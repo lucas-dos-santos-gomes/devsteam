9a3e33cf081ee736cb3e72281d02252c0aa190c0
Lucas dos Santos Gomes <lucasdev.programador@gmail.com>
2023-07-18 01:00:56 -0300

    Mudando configurações e metadados

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,13 +4,15 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>DevSteam: A sua loja online de games</title>
-        <meta name="description" content="DevSteam: A sua loja online de games" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="DevSteam: A sua loja online de games" />
+        <meta name="author" content="Lucas dos Santos Gomes" />
+        <meta name="keywords" content="steam, games, e-commerce, ecommerce, jogos" />
         <link rel="icon" href="/favicon.ico" />
+        <title>DevSteam: A sua loja online de games</title>
       </Head>
       <div>
-        DevStart: A sua loja online de games
+        DevSteam: A sua loja online de games
       </div>
     </>
   )
