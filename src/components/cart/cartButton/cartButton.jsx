117f02c94e5435d6fe33bc1ce2c9757783f0e8e7
Lucas dos Santos Gomes <lucasdev.programador@gmail.com>
2023-07-24 20:39:50 -0300

    Colocando o número no carrinho

--- src/components/cart/cartButton/cartButton.jsx
+++ src/components/cart/cartButton/cartButton.jsx
@@ -1,13 +1,17 @@
 import { useState } from 'react';
 import { BsCart4 } from 'react-icons/bs';
+import { useRecoilValue } from 'recoil';
+import { cartState } from '@/atoms/cart';
 import styles from './cartButton.module.css';
 import CartMenu from '@/components/cart/cartMenu/cartMenu';
 
 export default function CartButton() {
   const [open, setOpen] = useState(false);
+  const cart = useRecoilValue(cartState);
   return (
-    <div className={`${styles.cartcontainer} ${open && styles.clicked}`}>
-      <BsCart4 className={styles.icon} size={40} onClick={() => setOpen(!open)} />
+    <div className={`${styles.cartcontainer} ${open && styles.clicked}`} onClick={() => setOpen(!open)}>
+      <BsCart4 size={40} />
+      {cart.length == 0 || <div className={styles.number}>{cart.length}</div>}
       {open && <CartMenu />}
     </div>
   );
