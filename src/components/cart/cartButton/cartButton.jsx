9b91a39f93780ca7fc82680e3041b698f5281f72
Lucas dos Santos Gomes <lucasdev.programador@gmail.com>
2023-07-25 23:11:39 -0300

    Resolvendo bug do clique no carrinho

--- src/components/cart/cartButton/cartButton.jsx
+++ src/components/cart/cartButton/cartButton.jsx
@@ -9,8 +9,8 @@ export default function CartButton() {
   const [open, setOpen] = useState(false);
   const cart = useRecoilValue(cartState);
   return (
-    <div className={`${styles.cartcontainer} ${open && styles.clicked}`} onClick={() => setOpen(!open)}>
-      <BsCart4 size={40} />
+    <div className={`${styles.cartcontainer} ${open && styles.clicked}`}>
+      <BsCart4 size={40} onClick={() => setOpen(!open)} />
       {cart.length == 0 || <div className={styles.number}>{cart.length}</div>}
       {open && <CartMenu />}
     </div>
