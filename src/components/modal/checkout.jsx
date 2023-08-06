15eb2030789aedda3960e135d1248f2bf2543f32
Lucas dos Santos Gomes <lucasdev.programador@gmail.com>
2023-08-06 16:02:08 -0300

    Finalizando estilização do modal de checkout

--- src/components/modal/checkout.jsx
+++ src/components/modal/checkout.jsx
@@ -1,49 +1,42 @@
-import * as React from 'react';
+import Image from 'next/image';
 import Button from '@mui/material/Button';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
+import { useRecoilState } from 'recoil';
 
-export default function AlertDialog() {
-  const [open, setOpen] = React.useState(false);
+import { openCheckoutState } from '@/atoms/openCheckout';
 
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
+import styles from './checkout.module.css';
 
-  const handleClose = () => {
-    setOpen(false);
+export default function Checkout() {
+  const [openCheckout, setOpenCheckout] = useRecoilState(openCheckoutState);
+
+  const handleCloseModal = () => {
+    setOpenCheckout(false);
   };
 
   return (
-    <div>
-      <Button variant="outlined" onClick={handleClickOpen}>
-        Open alert dialog
-      </Button>
-      <Dialog
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="alert-dialog-title"
-        aria-describedby="alert-dialog-description"
-      >
-        <DialogTitle id="alert-dialog-title">
-          {"Use Google's location service?"}
-        </DialogTitle>
-        <DialogContent>
-          <DialogContentText id="alert-dialog-description">
-            Let Google help apps determine location. This means sending anonymous
-            location data to Google, even when no apps are running.
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose}>Disagree</Button>
-          <Button onClick={handleClose} autoFocus>
-            Agree
-          </Button>
-        </DialogActions>
-      </Dialog>
-    </div>
+    <Dialog
+      open={openCheckout}
+      onClose={handleCloseModal}
+      aria-labelledby="alert-dialog-title"
+      aria-describedby="alert-dialog-description"
+    >
+      <Image className={styles.image} src="/confirm.png" alt="Ícone de confirmação" width={120} height={120} />
+      <DialogTitle id="alert-dialog-title">
+        {"Pronto!"}
+      </DialogTitle>
+      <DialogContent>
+        <DialogContentText id="alert-dialog-description">
+          Sua compra foi efetuada com sucesso!
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleCloseModal} autoFocus>Fechar</Button>
+      </DialogActions>
+    </Dialog>
   );
 }
